5731e70380d07a93d431a08a3fe465af96df5086
Ordaricc <moriniriccardo97@gmail.com>
2020-12-15 10:33:13 +0100

    Smaller improvements

--- AudioTypes Example/Assets/Scripts/AudioManager.cs
+++ AudioTypes Example/Assets/Scripts/AudioManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
-    
+
+    [SerializeField] private AudioMixerGroup musicMixerGroup;
+    [SerializeField] private AudioMixerGroup soundEffectsMixerGroup;
     [SerializeField] private Sound[] sounds;
 
     private void Awake()
@@ -17,6 +20,17 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.audioClip;
             s.source.loop = s.isLoop;
             s.source.volume = s.volume;
+
+            switch (s.audioType)
+            {
+                case Sound.AudioTypes.soundEffect:
+                    s.source.outputAudioMixerGroup = soundEffectsMixerGroup;
+                    break;
+
+                case Sound.AudioTypes.music:
+                    s.source.outputAudioMixerGroup = musicMixerGroup;
+                    break;
+            }
 
             if (s.playOnAwake)
                 s.source.Play();
@@ -45,23 +59,9 @@ public class AudioManager : MonoBehaviour
         s.source.Stop();
     }
 
-    public void UpdateVolumeOfPlayingClips()
+    public void UpdateMixerVolume()
     {
-        foreach (Sound s in sounds)
-        {
-            float newVolume = s.volume;
-            if (s.audioType == Sound.AudioTypes.music)
-            {
-                newVolume *= AudioOptionsManager.musicVolume;
-                newVolume /= 100;
-            }
-            else if (s.audioType == Sound.AudioTypes.sound)
-            {
-                newVolume *= AudioOptionsManager.effectsVolume;
-                newVolume /= 100;
-            }
-
-            s.source.volume = newVolume;
-        }
+        musicMixerGroup.audioMixer.SetFloat("Music Volume", Mathf.Log10(AudioOptionsManager.musicVolume) * 20);
+        soundEffectsMixerGroup.audioMixer.SetFloat("Sound Effects Volume", Mathf.Log10(AudioOptionsManager.soundEffectsVolume) * 20);
     }
 }
--- AudioTypes Example/Assets/Scripts/Sound.cs
+++ AudioTypes Example/Assets/Scripts/Sound.cs
@@ -3,13 +3,13 @@
 [System.Serializable]
 public class Sound
 {
-    public enum AudioTypes { sound, music }
+    public enum AudioTypes { soundEffect, music }
+    public AudioTypes audioType;
 
     [HideInInspector] public AudioSource source;
     public string clipName;
     public AudioClip audioClip;
     public bool isLoop;
-    public AudioTypes audioType;
     public bool playOnAwake;
 
     [Range(0, 1)]
